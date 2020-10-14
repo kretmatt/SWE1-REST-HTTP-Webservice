cea4b03f1f736a616e9790ef8974c8db515a6976
Kretschi <if20b196@technikum-wien.at>
2020-10-14 13:37:32 +0200

    Init REST HTTP Webservice Project

--- SWE1-REST-HTTP-Webservice/Program.cs
+++ SWE1-REST-HTTP-Webservice/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace SWE1_REST_HTTP_Webservice
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+        }
     }
-  }
 }
