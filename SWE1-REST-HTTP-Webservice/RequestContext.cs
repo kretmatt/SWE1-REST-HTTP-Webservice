eeacb20e0d8aef1b6e178705c42ee8200959785f
kretmatt <mrtthias@gmail.com>
2020-11-02 23:55:17 +0100

    Add RequestContext unit tests and RequestContext class

---
+++ SWE1-REST-HTTP-Webservice/RequestContext.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace SWE1_REST_HTTP_Webservice
+{
+    public class RequestContext
+    {
+        public String Type { get; set; }
+        public String URL { get; set; }
+        public String HTTPVersion { get; set; }
+        public List<HttpHeaderPair> HeaderPairs { get; private set; }
+        public String Body { get; set; }
+        
+        private RequestContext(String type,String url,String httpVersion)
+        {
+            Type = type;
+            URL = url;
+            HTTPVersion = httpVersion;
+            HeaderPairs = new List<HttpHeaderPair>();
+        }
+
+        public void AddHeader(String headerLine)
+        {
+            String[] headerPairParts = headerLine.Split(' ');
+            HeaderPairs.Add(new HttpHeaderPair(headerPairParts[0].Trim(':'),headerPairParts[1]));
+        }
+        
+        public static RequestContext GetBaseRequest(String request)
+        {
+            if (String.IsNullOrEmpty(request))
+                return null;
+
+            String[] tokens = request.Split(' ');
+            String type = tokens[0];
+            String url = tokens[1];
+            String httpVersion = tokens[2];
+            
+            return new RequestContext(type,url,httpVersion);
+        }
+
+        public override string ToString()
+        {
+            string requestString = String.Format("{0} {1} {2}\n",Type,URL,HTTPVersion);
+            HeaderPairs.ForEach(hp=> requestString+=String.Format("{0}\n",hp.ToString()));
+            return String.Format("{0}\n{1}\n",requestString,Body);
+        }
+    }
+}
