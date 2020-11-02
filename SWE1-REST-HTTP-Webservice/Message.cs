584772863d2ed9558e2f3d1edb083cf1cfe402a2
kretmatt <mrtthias@gmail.com>
2020-11-03 00:18:33 +0100

    Add message class

---
+++ SWE1-REST-HTTP-Webservice/Message.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SWE1_REST_HTTP_Webservice
+{
+    class Message
+    {
+        public int Id { get; set; }
+        public String Content { get; set; }
+        public DateTime SentDate { get; set; }
+    }
+}
