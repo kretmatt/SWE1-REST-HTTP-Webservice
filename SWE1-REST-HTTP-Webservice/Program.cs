a8b751e6e1a768ec0e07313103e0bfe76be5d0e0
kretmatt <mrtthias@gmail.com>
2020-11-03 01:28:32 +0100

    Fix program.cs file

--- SWE1-REST-HTTP-Webservice/Program.cs
+++ SWE1-REST-HTTP-Webservice/Program.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting.Messaging;
-using System.Text.RegularExpressions;
-using System.Threading;
-
-namespace SWE1_REST_HTTP_Webservice
+﻿namespace SWE1_REST_HTTP_Webservice
 {
     internal class Program
     {
